e2a2fd8b76f6e84af2de29ae1a192a761a9cbf0d
jikish <jikish2103dk@gmail.com>
2019-11-19 18:39:45 +0600

    Part-2 Task-16

---
+++ task-16.py
@@ -0,0 +1,10 @@
+Your_kg = int(input('Vedite svoi ves: '))
+# moon = Your_kg * 0.165
+# print(moon)
+moon_kg = 0
+round(moon_kg, 2)
+
+for i in range(15):
+    Your_kg += 1
+    moon_kg = Your_kg * 0.165
+    print(moon_kg)
